ed452502e4906c2d8dbaf140b0e596f7c350e759
Ahad Islam <ahad.islam624@gmail.com>
2020-04-25 23:57:32 -0400

    creating codable classes with @Published, and encoding/decoding via property wrappers and coding keys

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,14 +8,35 @@
 
 import SwiftUI
 
+class User: ObservableObject, Codable {
+  
+  @Published var name = "Paul Hudson"
+  
+  required init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    name = try container.decode(String.self, forKey: .name)
+  }
+  
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(name, forKey: .name)
+  }
+  
+  enum CodingKeys: CodingKey {
+    case name
+  }
+  
+  
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+  var body: some View {
+    Text("Hello, World!")
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
