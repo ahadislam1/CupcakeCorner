bd3293a9cdbf5e2b9c9c742ded7aaef1854d76c5
Ahad Islam <ahad.islam624@gmail.com>
2020-04-26 12:14:30 -0400

    creating a confirmation message and showing an alert

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -11,6 +11,12 @@ import SwiftUI
 struct CheckoutView: View {
   @ObservedObject var order: Order
   
+  @State private var confirmationMessage = ""
+  @State private var showingConfirmation = false
+  
+  @State private var errorMessage = ""
+  @State private var showingError = false
+  
   var body: some View {
     GeometryReader { geo in
       ScrollView {
@@ -24,14 +30,51 @@ struct CheckoutView: View {
             .font(.title)
           
           Button("Place Order") {
-            // place the order
+            self.placeOrder()
           }
           .padding()
           
         }
       }
     }
-    .navigationBarTitle("Check out", displayMode: .inline)
+    .alert(isPresented: $showingConfirmation) {
+      Alert(title: Text("Thank you"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+    }
+    .alert(isPresented: $showingError) {
+      Alert(title: Text("Error"), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+    }
+        .navigationBarTitle("Check out", displayMode: .inline)
+    }
+    
+    func placeOrder() {
+      guard let encoded = try? JSONEncoder().encode(order) else {
+        print("Failed to encode order.")
+        return
+      }
+      
+      let url = URL(string: "https://reqres.in/api/cupcakes")!
+      var request = URLRequest(url: url)
+      request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+      request.httpMethod = "POST"
+      request.httpBody = encoded
+      
+      URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data else {
+          print("Something went wrong: \(error?.localizedDescription)")
+          return
+        }
+        
+        if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+          self.confirmationMessage = "Your order for \(decodedOrder.quantity) \(Order.types[decodedOrder.type].lowercased()) is on its way!"
+          self.showingConfirmation = true
+        } else {
+          self.errorMessage = "No internet"
+          self.showingError = true
+      }
+    }.resume()
+    
+    
+    
   }
 }
 
