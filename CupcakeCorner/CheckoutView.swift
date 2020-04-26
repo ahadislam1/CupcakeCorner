ea1d7c31b6fc28821fd570709125fd8218c0b005
Ahad Islam <ahad.islam624@gmail.com>
2020-04-26 11:37:39 -0400

    adding computed properties to data model to separate view logic, using geometry reader to scale images and etc.

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -11,13 +11,32 @@ import SwiftUI
 struct CheckoutView: View {
   @ObservedObject var order: Order
   
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    GeometryReader { geo in
+      ScrollView {
+        VStack {
+          Image("cupcakes")
+            .resizable()
+            .scaledToFit()
+            .frame(width: geo.size.width)
+          
+          Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+            .font(.title)
+          
+          Button("Place Order") {
+            // place the order
+          }
+          .padding()
+          
+        }
+      }
     }
+    .navigationBarTitle("Check out", displayMode: .inline)
+  }
 }
 
 struct CheckoutView_Previews: PreviewProvider {
-    static var previews: some View {
-        CheckoutView(order: Order())
-    }
+  static var previews: some View {
+    CheckoutView(order: Order())
+  }
 }
